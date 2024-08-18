abb8e141ef7ec01ef6f9aaae490d216f3f3172b2
PradeepTiwarii <pradeeptiwarig89@gmail.com>
2024-08-18 17:55:05 +0530

    add all file

---
+++ models/review.js
@@ -0,0 +1,19 @@
+const { string } = require("joi");
+const mongoose = require("mongoose");
+const { type } = require("os");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment: String,
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5
+    },
+    createdAt: {
+        type: Date,
+        default : Date.now(),
+    }
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
